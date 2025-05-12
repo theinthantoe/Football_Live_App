36704684430044cf2ef22b9c9d1cf55865c989fc
Thein Than Toe <theinthantoe15@gmail.com>
2025-05-12 22:45:32 +0630

    fixed cors error

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as process from 'node:process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['content-type', 'Authorization'],
